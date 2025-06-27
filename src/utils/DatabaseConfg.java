69459860366c4d7f018a13d05ff9d178bab6640c
Gabriel3465 <gabriel2_23@alu.uabcs.mx>
2025-06-26 18:31:46 -0700

    prueba

--- src/utils/DatabaseConfg.java
+++ src/utils/DatabaseConfg.java
@@ -2,12 +2,12 @@ package utils;
 
 public class DatabaseConfg {
 
-	public static final String URL = "jdbc:mysql://127.0.0.1:3306/base_de_datos_renta";
-	public static final String USER = "root";
-	public static final String PASSWORD = "8163264gA?¡";
+//	public static final String URL = "jdbc:mysql://127.0.0.1:3306/base_de_datos_renta";
+//	public static final String USER = "root";
+//	public static final String PASSWORD = "";
 
-//	public static final String URL = "jdbc:mysql://sql.freedb.tech:3306/freedb_Base_de_datos_renta?useSSL=false";
-//	public static final String USER = "freedb_G_user";
-//	public static final String PASSWORD = "%eeFW9csb4$?Dcj";
+	public static final String URL = "jdbc:mysql://sql.freedb.tech:3306/freedb_Base_de_datos_renta?useSSL=false";
+	public static final String USER = "freedb_G_user";
+	public static final String PASSWORD = "%eeFW9csb4$?Dcj";
 	
 }
